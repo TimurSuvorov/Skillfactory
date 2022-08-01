3e975fee544e70ca22c631e2c1805cfd2c54dff7
RoboTTimur <suvorovt@gmail.com>
2022-08-01 18:38:06 +0300

    add Ship class

--- C2.5 SeaFight (Homework).py
+++ C2.5 SeaFight (Homework).py
@@ -3,7 +3,6 @@ class BoardOutException(Exception):
     def __str__(self):
         return "Удар вне поля боя!"
 
-
 class BoardUsedException(Exception):
     def __str__(self):
         return "Удар в те же координаты"
@@ -21,3 +20,29 @@ class Dot:
 
     def __repr__(self):
         return f"Dot ({self.x}, {self.y})"
+
+#Определение класса корабля (длина, расположение, жизни)
+class Ship:
+
+    def __init__(self, length, stpos, direction):
+        self.length = length
+        self.stpos = stpos
+        self.direction = direction
+        self.health = length
+
+    @property
+    def dots(self):
+        ships_dots = []
+        # В self.stpos передается объект из Dot
+
+        for i in range(self.length):
+            shipdot_x = self.stpos.x
+            shipdot_y = self.stpos.y
+            if self.direction == "v":
+                shipdot_x += i
+            if self.direction == "h":
+                shipdot_y += i
+
+            ships_dots.append(Dot(shipdot_x, shipdot_y))
+        return ships_dots
+
