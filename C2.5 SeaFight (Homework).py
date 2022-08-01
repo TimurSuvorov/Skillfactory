c15ce79f97181b6bb6e9c631b394482e9d76eccf
TSuvorov <suvorovt@gmail.com>
2022-08-01 16:29:01 +0300

    add Dot class

--- C2.5 SeaFight (Homework).py
+++ C2.5 SeaFight (Homework).py
@@ -7,3 +7,17 @@ class BoardOutException(Exception):
 class BoardUsedException(Exception):
     def __str__(self):
         return "Удар в те же координаты"
+
+
+# Определение класса точек на поле
+class Dot:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other_dot):
+        return self.x == other_dot.x and self.y == other_dot.y
+
+    def __repr__(self):
+        return f"Dot ({self.x}, {self.y})"
