1e68f66ac71f77f3e1c9b58ecdb63331c9beb57f
RoboTTimur <suvorovt@gmail.com>
2022-09-19 00:23:44 +0300

    models corrections

--- Module_D/NewsPortal/news/models.py
+++ Module_D/NewsPortal/news/models.py
@@ -3,45 +3,49 @@ from django.contrib.auth.models import User
 
 
 class Author(models.Model):
-    rate = models.IntegerField(default=0)
+    rating = models.SmallIntegerField(default=0)
     username = models.OneToOneField(User,
                                     on_delete=models.CASCADE,
                                     primary_key=True  # Первичный ключ будет тот же, что и внешний ключ
                                     )
 
-# newrate calculating
+# newrating calculating
 # for author_ in Author.objects.all().values_list('username_id', flat=True):
-#     newrate = sum(Post.objects.filter(author_id = author_).values_list('rate', flat=True))*3 + \
-#               sum(Comment.objects.filter(author_id = author_).values_list('rate', flat=True)) + \
-#               sum(Comment.objects.filter(post_id__author_id = author_).values_list('rate', flat=True))
+#     newrating = sum(Post.objects.filter(author_id = author_).values_list('rating', flat=True))*3 + \
+#               sum(Comment.objects.filter(author_id = author_).values_list('rating', flat=True)) + \
+#               sum(Comment.objects.filter(post_id__author_id = author_).values_list('rating', flat=True))
 #     au = Author.objects.get(username_id=author_)
-#     au.update_rating(newrate)
+#     au.update_rating(newrating)
 #     au.save()
 
 
-    def update_rating(self, newrate):
-        self.rate = newrate
+    def update_rating(self, newrating):
+        self.rating = newrating
 
 class Category(models.Model):
-    catname = models.CharField(max_length=255,
+    catname = models.CharField(max_length=64,
                                unique=True
                                )
 
 
 class Post(models.Model):
 
+    ARTICLE = 'AR'
+    NEWS = 'NW'
+
     TYPES = [
-        ('post', "Пост"),
-        ('news', "Новость")
+        (ARTICLE, "Пост"),
+        (NEWS, "Новость")
     ]
 
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=254)
     content = models.TextField()
-    rate = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0)
     type = models.CharField(max_length=10,
                             choices=TYPES,
                             blank=True,
-                            null=True
+                            null=True,
+                            default='post'
                             )
     cr_time = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='post')
@@ -51,10 +55,10 @@ class Post(models.Model):
         return self.content[:124] + "..."
 
     def like(self):
-        self.rate += 1
+        self.rating += 1
 
     def dislike(self):
-        self.rate -= 1
+        self.rating -= 1
 
 
 
@@ -68,11 +72,11 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=False)
     cr_time = models.DateTimeField(auto_now_add=True)
-    rate = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0)
 
     def like(self):
-        self.rate += 1
+        self.rating += 1
 
     def dislike(self):
-        self.rate -= 1
+        self.rating -= 1
 
