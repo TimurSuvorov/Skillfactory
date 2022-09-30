c0a2132aa62faaccf09098bccb189b7ba1cb1c30
TSuvorov <suvorovt@gmail.com>
2022-09-30 16:41:12 +0300

    view corrections

--- Module_D/NewsPortal/news/views.py
+++ Module_D/NewsPortal/news/views.py
@@ -18,9 +18,3 @@ class SpecialPost(DetailView):
     template_name = 'specialpost.html'
     context_object_name = 'specialpost'
     pk_url_kwarg = 'id'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        #r = Post.objects.filter(Post=self.model).values_list('postAuthor__author__username', flat=True)
-        #print(r)
-        return context
